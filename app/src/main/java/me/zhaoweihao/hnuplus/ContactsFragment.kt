d5f6e77f2b20475e78368181b51d62d40667263a
zhaoweihao <1028410337@qq.com>
2017-11-19 13:37:12 +0800

    delete anko sql

--- app/src/main/java/me/zhaoweihao/hnuplus/ContactsFragment.kt
+++ app/src/main/java/me/zhaoweihao/hnuplus/ContactsFragment.kt
@@ -7,10 +7,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.contacts_layout.*
-import me.zhaoweihao.hnuplus.SQL.database
-import org.jetbrains.anko.attempt
-import org.jetbrains.anko.db.*
 
 
 
@@ -34,40 +30,6 @@ class ContactsFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        /*
-        using kotlin-anko-sqlite & kotlin-android-extensions in fragment
-        thanks to those posts:
-        http://blog.csdn.net/cysion1989/article/details/72903002
-        https://antonioleiva.com/databases-anko-kotlin/
-        https://antonioleiva.com/kotlin-android-extensions/
-         */
-//        insert.setOnClickListener {
-//            activity.database.use {
-//                if (attempt {
-//                    var name: String = "John"
-//                    var age: Int = 13
-//                    insert("Person",
-//                            "age" to age,
-//                            "name" to name
-//                    )
-//                    display.text = "insert(Person,age to $age,name to $name,address to )"
-//                }.isError) {
-//                    display.text = "错误,可能不存在表"
-//                }
-//            }
-//        }
-//        query.setOnClickListener {
-//            activity.database.use {
-//                if (attempt {
-//                    val whereArgs = select("Person", "name", "age")
-//                    val parseList = whereArgs.parseList(classParser<Person>())
-//
-//                    display.text = parseList.toString()
-//                }.isError) {
-//                    display.text = "错误,可能不存在表"
-//                }
-//            }
-//        }
 
     }
 
--- app/src/main/java/me/zhaoweihao/hnuplus/DB/DataSave.kt
+++
@@ -1,8 +0,0 @@
-package me.zhaoweihao.hnuplus.DB
-
-import me.zhaoweihao.hnuplus.JavaBean.Post
-
-/**
- * Created by Administrator on 2017/11/13.
- */
-data class DataSave(val postAuthor:String,val postContent:String,val objectID:String,val authorID:String)
